9acac1a7d71a848cb6090f1285f9bb45b1d717e0
Palmer Hinchliffe <palmer.hinchliffe@rotamaster.co.uk>
2024-01-16 22:19:09 +0000

    refactor: moving logic from controller to service

--- src/api/imitatee/imitatee.controller.ts
+++ src/api/imitatee/imitatee.controller.ts
@@ -1,51 +1,23 @@
 import type { Request, Response, NextFunction } from 'express';
-import { FilterQuery, FindOptions, RequestContext } from '@mikro-orm/core';
-import { Imitatee } from './imitatee.entity.js';
 import ApiException from '../../core/ApiException.js';
 import { IndexPaginatedEntityResponse } from '../../core/types.js';
-import { Fly } from '../fly/fly.entity.js';
 import { ImitateeResourceModel } from './imitatee.types.js';
-import { mapEntityDbModelToResourceModel } from '../../core/utils.js';
 import { FlyResourceModel } from '../fly/fly.types.js';
 import { userHasPermission } from '../user/user.service.js';
 import { UserPermissionName } from '../userPermission/userPermission.constants.js';
+import * as imitateeService from './imitatee.service.js';
 
 export const indexImitatees = async (
     req: Request,
     res: Response<IndexPaginatedEntityResponse<ImitateeResourceModel>>,
     next: NextFunction,
 ): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
-    const pageNumber = Number(req.query.pageNumber);
-    const pageSize = Number(req.query.pageSize);
-    const findOptions: FindOptions<Imitatee> = {
-        offset: (pageNumber - 1) * pageSize,
-        limit: pageSize,
-        orderBy: { name: 'ASC' },
-    };
-
-    const results = await repository?.findAndCount({}, findOptions);
-
-    if (!results) {
-        const error = new ApiException({ message: 'Unable to fetch imitatees' });
-        return next(error);
+    try {
+        const result = await imitateeService.indexImitatees(Number(req.query.pageNumber), Number(req.query.pageSize));
+        res.json(result);
+    } catch (e) {
+        next(e);
     }
-
-    const imitatees = results[0];
-    const totalItems = results[1];
-    const totalPages = Math.ceil(totalItems / pageSize);
-
-    res.setHeader('Content-Range', `bytes 0-${totalItems}/*`);
-    res.json({
-        metadata: {
-            totalItems,
-            pageNumber,
-            pageSize,
-            totalPages,
-        },
-        results: imitatees.map((x) => mapEntityDbModelToResourceModel(x)),
-    });
 };
 
 export const getImitatee = async (
@@ -53,21 +25,19 @@ export const getImitatee = async (
     res: Response<ImitateeResourceModel>,
     next: NextFunction,
 ): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
-    const result = await repository?.findOne({ externalId: req.params.id });
-
-    if (!result) {
-        const error = new ApiException({ message: `Cannot find imitatee using id: ${req.params.id}`, status: 409 });
-        return next(error);
+    try {
+        const result = await imitateeService.getImitatee(req.params.id);
+        res.json(result);
+    } catch (e) {
+        next(e);
     }
-
-    res.json(mapEntityDbModelToResourceModel(result));
 };
 
-export const createImitatee = async (req: Request, res: Response<string>, next: NextFunction): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
+export const createImitatee = async (
+    req: Request,
+    res: Response<ImitateeResourceModel['id']>,
+    next: NextFunction,
+): Promise<void> => {
     const userId = req.body.user.userId;
     const hasPermission = await userHasPermission(userId, UserPermissionName.CREATE);
 
@@ -79,17 +49,12 @@ export const createImitatee = async (req: Request, res: Response<string>, next: 
         return next(error);
     }
 
-    const exists = await repository?.exists(req.body.name);
-
-    if (exists) {
-        const error = new ApiException({ message: 'Imitatee name already exists', status: 409 });
-        return next(error);
+    try {
+        const externalId: string = await imitateeService.createImitatee(req.body.name, req.body.description);
+        res.json(externalId);
+    } catch (e) {
+        next(e);
     }
-
-    const imitatee = new Imitatee(req.body.name, req.body.description);
-
-    await em?.persist(imitatee).flush();
-    res.json(imitatee.externalId);
 };
 
 export const updateImitatee = async (
@@ -97,8 +62,6 @@ export const updateImitatee = async (
     res: Response<ImitateeResourceModel>,
     next: NextFunction,
 ): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
     const userId = req.body.user.userId;
     const hasPermission = await userHasPermission(userId, UserPermissionName.CREATE);
 
@@ -110,23 +73,19 @@ export const updateImitatee = async (
         return next(error);
     }
 
-    const imitatee = await repository?.findOne({ externalId: req.params.id });
-
-    if (!imitatee) {
-        const error = new ApiException({ message: `Cannot find imitatee using id: ${req.params.id}`, status: 409 });
-        return next(error);
+    try {
+        const imitatee = await imitateeService.updateImitatee({
+            id: req.params.id,
+            name: req.body.name,
+            description: req.body.description,
+        });
+        res.json(imitatee);
+    } catch (e) {
+        next(e);
     }
-
-    imitatee.name = req.body.name;
-    imitatee.description = req.body.description;
-
-    await em?.flush();
-    res.json(mapEntityDbModelToResourceModel(imitatee));
 };
 
 export const deleteImitatee = async (req: Request, res: Response<string>, next: NextFunction): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
     const userId = req.body.user.userId;
     const hasPermission = await userHasPermission(userId, UserPermissionName.DELETE);
 
@@ -138,14 +97,12 @@ export const deleteImitatee = async (req: Request, res: Response<string>, next: 
         return next(error);
     }
 
-    const result = await repository?.nativeDelete({ externalId: req.params.id });
-
-    if (result === 0) {
-        const error = new ApiException({ message: `Cannot find imitatee using id: ${req.params.id}` });
-        return next(error);
+    try {
+        await imitateeService.deleteImitatee(req.params.id);
+        res.send('OK');
+    } catch (e) {
+        next(e);
     }
-
-    res.json('OK');
 };
 
 export const indexFliesByImitatee = async (
@@ -153,56 +110,14 @@ export const indexFliesByImitatee = async (
     res: Response<IndexPaginatedEntityResponse<FlyResourceModel>>,
     next: NextFunction,
 ): Promise<void> => {
-    const em = RequestContext.getEntityManager();
-    const repository = em?.getRepository(Imitatee);
-    const flyRepository = em?.getRepository(Fly);
-
-    const imitatee = await repository?.findOne({ externalId: req.params.id });
-
-    if (!imitatee) {
-        const error = new ApiException({ message: `Cannot find imitatee using id: ${req.params.id}`, status: 409 });
-        return next(error);
+    try {
+        const results = await imitateeService.indexFliesByImitatee({
+            id: req.params.id,
+            pageNumber: req.query.pageNumber,
+            pageSize: req.query.pageSize,
+        });
+        res.send(results);
+    } catch (e) {
+        next(e);
     }
-
-    const pageNumber = Number(req.query.pageNumber);
-    const pageSize = Number(req.query.pageSize);
-    const filterQuery: FilterQuery<Fly> = { types: imitatee.id };
-    const findOptions: FindOptions<Fly> = {
-        offset: (pageNumber - 1) * pageSize,
-        limit: pageSize,
-        populate: ['types', 'imitatees'] as never,
-        orderBy: { name: 'ASC' },
-    };
-
-    const results = await flyRepository?.findAndCount(filterQuery, findOptions);
-
-    if (!results) {
-        const error = new ApiException({ message: 'Unable to fetch flies' });
-        return next(error);
-    }
-
-    const flies = results[0];
-    const totalItems = results[1];
-    const totalPages = Math.ceil(totalItems / pageSize);
-
-    const mappedResults: Array<FlyResourceModel> = flies.map((x) => {
-        const { externalId: _externalId, id: _id, ...entityModel } = x;
-        return {
-            ...entityModel,
-            id: x.externalId,
-            imitatees: x.imitatees.toArray().map(mapEntityDbModelToResourceModel),
-            types: x.types.toArray().map(mapEntityDbModelToResourceModel),
-        };
-    });
-
-    res.setHeader('Content-Range', `bytes 0-${totalItems}/*`);
-    res.json({
-        metadata: {
-            totalItems,
-            pageNumber,
-            pageSize,
-            totalPages,
-        },
-        results: mappedResults,
-    });
 };
